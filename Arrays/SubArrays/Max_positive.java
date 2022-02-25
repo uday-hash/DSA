d5b50aee925353d595bdde0dc9b44a8c0e99a8c8
uday-hash <ukm160@gmail.com>
2022-02-26 01:02:00 +0530

    modified

--- Arrays/SubArrays/Max_positive.java
+++ Arrays/SubArrays/Max_positive.java
@@ -1,19 +1,38 @@
 package Arrays.SubArrays;
 
-public class Max_positive {
-    public static void main(String[] args) {
-        int[] a = {5,6,-1,7,8};
-        int n = a.length;
-       // int res = 0;
+import java.util.*;
 
-        for(int i = 0; i < n ; i++) {
-            for(int j = i; j < n; j++ ){
-                for(int k = j; k <= j; k++){
-                    System.out.print(a[k]);
-                }
-            }
-            System.out.println();
+public class Max_positive {
+    public static void main(String[] args){
+        int[] arr = {-1,2,5,-1,5,7};
+        int n = 6;
+        int[] out = maxArray1(arr, n);
+        for (int j : out) {
+            System.out.print(j);
         }
-        //System.out.println(A);
+    }
+
+    static int[] maxArray1(int[] arr, int n){
+        HashMap<Integer, Integer> load = new HashMap<>();
+        for(int i=0; i< n; i++){
+            int j=0;
+            while(arr[i+j]<n && arr[i+j]>=0){
+                j++;
+            }
+            load.put(i, j);
+        }
+//      fetch max index from hashMap
+        int maxIndex = 0;
+        for(int i=0; i< n; i++){
+            if(load.get(i)>load.get(maxIndex)){
+                maxIndex=i;
+            }
+        }
+//      copy max array from provided array
+        int[] out = new int[load.get(maxIndex)];
+        for(int j=0; j<load.get(maxIndex); j++){
+            out[j]=arr[maxIndex+j];
+        }
+        return out;
     }
 }
