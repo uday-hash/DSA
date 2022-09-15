0b38d75da8fde63d69aa9cce6f41d6985f604964
Uday_hash <ukm160@gmail.com>
2022-09-16 04:08:20 +0530

    DSA

---
+++ Trees/Path_to_given_node.java
@@ -0,0 +1,30 @@
+/*Problem Description
+Given a Binary Tree A containing N nodes, you need to find the path from Root to a given node B.
+
+NOTE:
+
+No two nodes in the tree have the same data values.
+You can assume that B is present in tree A and a path always exists. */
+
+package Trees;
+
+import java.util.ArrayList;
+
+public class Path_to_given_node {
+    public ArrayList<Integer> solve(TreeNode A, int B) {
+        
+    }
+    
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) {
+      val = x;
+      left=null;
+      right=null;
+    }
+}
+  
